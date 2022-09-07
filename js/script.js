44c939da231724e133a832e3aa4ddea7da3ebee2
MichaellRodrigues <79985947+MichaellRodrigues@users.noreply.github.com>
2022-09-07 12:31:58 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
+import { IMC, notNumber } from './utils.js'
+
+// variáveis
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+//const modalWrapper = document.querySelector('.modal-wrapper')
+//const modalMessage = document.querySelector('.modal .title span')
+//const modalBtnClose = document.querySelector('.modal button.close')
+
+// 3 maneiras de criar e atribuir função a um evento
+//form.onsubmit = () => {}
+
+//form.onsubmit = handleSubmit
+//function handleSubmit() {}
+
+// form.onsubmit = function() {}
+form.onsubmit = event => {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  const showAlertError = notNumber(weight) || notNumber(height)
+  if (showAlertError) {
+    AlertError.open()
+    return
+  }
+  AlertError.close()
+
+  const result = IMC(weight, height)
+  displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
+  const message = `seu IMC é de ${result}`
+
+  Modal.message.innerText = message
+  Modal.open()
+}
+
+inputWeight.oninput = () => {
+  AlertError.close()
+}
+inputHeight.oninput = () => {
+  AlertError.close()
+}
